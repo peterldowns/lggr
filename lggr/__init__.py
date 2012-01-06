d6b29fe19d1e4d4fb3ab1a1dc7cf1484090a0006
Peter Downs <peter.l.downs@gmail.com>
2012-01-05 20:54:55 -0500

    Redesigns the log() function's internal workings.
    New:
    - Creates `log_records` of information, just like logging.py.
    - Standardizes an over-rideable default logging format.
    - Allows 'extra' information to be passed in to default_fmt, just like
      logging.py
    - Allows log functions to focus on logging - only sends them the final
      log message.
    
    Bug fixes:
    - no longer adds a message to history for each level of logging

--- lggr/__init__.py
+++ lggr/__init__.py
@@ -1,10 +1,12 @@
 from coroutine import Coroutine
+from coroutine import CoroutineProcess
+import threading
 import inspect
 import smtplib
 import socket
-import types
 import time
 import sys
+import os
 
 EXCEPTION = "EXCEPTION"
 CRITICAL = "CRITICAL"
@@ -17,13 +19,14 @@ class Lggr():
 	""" Simplified logging. Dispatches messages to any type of
 		logging function you want to write, all it has to support
 		is send() and close(). """
-	def __init__(self):
+	def __init__(self, default_fmt="{asctime} ({level}) {log_message}"):
 		self.config = {
-				EXCEPTION: set(),
+				EXCEPTION: set(), # these are different levels of logger functions
 				CRITICAL: set(),
 				ERROR: set(),
 				WARNING: set(),
-				INFO: set()
+				INFO: set(),
+				"default_fmt": default_fmt
 			}
 		self.history = []
 		self.enabled = True
@@ -59,52 +62,85 @@ class Lggr():
 			item.close()
 		self.config[level].clear()
 	
-	def log(self, levels, fmt, *args, **kwargs):
+	def log(self, level, fmt, *args, **kwargs):
 		""" Send a log message to all of the logging functions
-			for each level in a given list, as well as adding the
+			for a given level as well as adding the
 			message to this logger instance's history. """
 		if not self.enabled:
 			return # Fail silently so that logging can easily be removed
 
-		message = fmt.format(*args, **kwargs) 
+		log_record = { # This is available information for logging functions.
+			#TODO:  proc_name, thread_name, filename, funcname, lineno, module, pathname
+			"args" : args,
+			"kwargs" : kwargs,
+			"asctime": time.asctime(), # TODO: actual specifier
+			"time" : time.time(),
+			"exc_info" : sys.exc_info(),
+			"level" : level,
+			"message_fmt" : fmt,
+			"log_message" : fmt.format(*args, **kwargs),
+			"proc_id" : os.getpid(),
+			"thread" : threading.currentThread(),
+			"default_fmt" : self.config['default_fmt'],
+		}
 
-		for lvl_name in levels:
-			self.history.append((time.time(), lvl_name, message))
-			log_funcs = self.config[lvl_name]
-			for lf in log_funcs.copy(): # so that we can edit the original while iterating
-				try:
-					lf.send(message)
-				except StopIteration: # already closed
-					self.remove(lvl_name, lf)
-					self.info("Logging function {} in level {} stopped.", lf, lvl_name)
+		if 'default_fmt' in kwargs:
+			log_record['default_fmt'] = kwargs['default_fmt'] # allow custom formats
+		if 'extra' in kwargs:
+			log_record.update(kwargs['extra']) # allow custom information to be sent in
+
+		logstr = log_record['default_fmt'].format(**log_record) #whoah.
+
+		self.history.append(logstr)
+
+		log_funcs = self.config[level]
+		to_remove = []
+		for lf in log_funcs:
+			try:
+				lf.send(logstr)
+			except StopIteration: # already closed, add it to the list to be deleted
+				to_remove.append(lf)
+		for lf in to_remove:
+			self.remove(level, lf)
+			self.info("Logging function {} in level {} stopped.", lf, level)
+
+	def exception(self, msg, *args, **kwargs):
+		""" Log a message with EXCEPTION level """
+		self.log(EXCEPTION, msg, *args, **kwargs)
 
 	def critical(self, msg, *args, **kwargs):
 		""" Log a message with CRITICAL level """
-		self.log([CRITICAL], msg, *args, **kwargs)
+		self.log(CRITICAL, msg, *args, **kwargs)
 
 	def error(self, msg, *args, **kwargs):
 		""" Log a message with ERROR level """
-		self.log([ERROR], msg, *args, **kwargs)
+		self.log(ERROR, msg, *args, **kwargs)
 
 	def warning(self, msg, *args, **kwargs):
 		""" Log a message with WARNING level """
-		self.log([WARNING], msg, *args, **kwargs)
+		self.log(WARNING, msg, *args, **kwargs)
 		
 	def info(self, msg, *args, **kwargs):
 		""" Log a message with INFO level """
-		self.log([INFO], msg, *args, **kwargs)
+		self.log(INFO, msg, *args, **kwargs)
+		
+	def multi(self, lvl_list, msg, *args, **kwargs):
+		""" Log a message at multiple levels"""
+		for level in lvl_list:
+			self.log(level, msg, *args, **kwargs)
+
 	def all(self, msg, *args, **kwargs):
-		""" Log a message at all levels """
-		self.log(ALL, msg, *args, **kwargs)
+		""" Log a message at every known log level """
+		self.multi(ALL, msg, *args, **kwargs)
 
 @Coroutine
 def Printer(open_file=sys.stdout, closing=False):
 	""" Prints items with a timestamp. """
-	message = "({0}): {1}\n"
 	try:
 		while True:
-			item = (yield)
-			open_file.write(message.format(time.asctime(), item))
+			logstr = (yield)
+			open_file.write(logstr)
+			open_file.write('\n') # new line
 	except GeneratorExit:
 		if closing:
 			try: open_file.close()
@@ -127,8 +163,8 @@ def SocketWriter(host, port, af=socket.AF_INET, st=socket.SOCK_STREAM):
 	s.connect(host, port)
 	try:
 		while True:
-			item = (yield)
-			s.send(message.format(time.asctime(), item))
+			logstr = (yield)
+			s.send(logstr)
 	except GeneratorExit:
 		s.close()
 
@@ -142,9 +178,9 @@ def Emailer(recipients, sender=None):
 	smtp = smtplib.SMTP('localhost')
 	try:
 		while True:
-			item = (yield)
+			logstr = (yield)
 			try:
-				smtp.sendmail(sender, recipients, item)
+				smtp.sendmail(sender, recipients, logstr)
 			except smtplib.SMTPExcpetion:
 				pass
 	except GeneratorExit:
