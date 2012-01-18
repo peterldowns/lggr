a75c0f500fb748ee6c9e81db4f9fdac7dcb3fde6
Peter Downs <peter.l.downs@gmail.com>
2012-01-18 14:35:26 -0500

    Updated the test file to show the new API

--- test.py
+++ test.py
@@ -1,24 +1,26 @@
 import lggr
 
-d = lggr.Lggr()
+d = lggr.Lggr() # create a logger object
 
-d.add(lggr.ALL, lggr.FilePrinter("output.log"))
+d.add(lggr.ALL, lggr.FilePrinter("~/output.log"))
 d.add(lggr.ALL, lggr.StderrPrinter())
 d.add(lggr.ALL, lggr.Printer())
 try:
-	d.add(lggr.ALL, lggr.Emailer(["peter.l.downs@gmail.com"], "peter"))
+	d.add(lggr.CRITICAL, lggr.Emailer(["peter.l.downs@gmail.com"], "peter"))
 except Exception as e:
 	print e
+	print "Could not create emailer :/"
 
 for level in lggr.ALL:
 	print "Level {}:".format(level)
 	for log_func in d.config[level]:
 		print '\t{}'.format(log_func)
 
-d.info("Hello, world!")
+d.debug("Hello, ")
+d.info("world!")
 d.warning("My name is {}", "Peter")
-d.error("Testing some {name} logging", name="ERROR")
-d.critical("Oh shit, nigel. Something is horribly wrong.")
+d.error("Testing some {name} logging", {"name":"ERROR"})
+d.critical("Oh shit. Something is horribly wrong.")
 d.all("This goes out to all of my friends")
 d.multi([lggr.WARNING, lggr.INFO], "This is only going to some of my friends")
 
@@ -27,9 +29,7 @@ d.clear(lggr.WARNING)
 d.clear(lggr.INFO)
 
 d.info("Testing....")
-d.error("Testing {0} {1} {2}", "another", "stupid", "thing")
-d.info("Here's something new. I changed the default {}!", "format",
-		default_fmt="[{descriptor} @ {asctime}] {log_message}",
-		extra={"descriptor":"custom-log-message"})
+d.error("Testing {} {} {}", "another", "stupid", "thing")
 
 d.close()
+d.log(lggr.ALL, "Help?")
