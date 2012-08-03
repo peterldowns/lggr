cd116b2e856d1c96b2de78b787d150434be9de21
Peter Downs <peter.l.downs@gmail.com>
2012-08-03 00:15:33 -0700

    Adds setup.cfg and setup.py for PYPI

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+    name = 'lggr',
+    packages = ['lggr'],
+    version = '0.1.0',
+    description = 'Python Logging For Humans',
+    author = 'Peter Downs',
+    author_email = 'peterldowns@gmail.com',
+    url = 'https://github.com/peterldowns/lggr',
+    download_url = 'https://github.com/peterldowns/lggr/tarball/v0.1.0',
+    keywords = ['logging', 'log', 'coroutine'],
+    classifiers = [
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Logging',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    long_description = open('README.md').read(),
+)
