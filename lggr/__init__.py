df648b89d3550739a20193549240afed8093546f
Peter Downs <peterldowns@gmail.com>
2012-12-02 20:37:40 -0800

    Version bump to 0.2.0.
    Changed to 0.2.x instead of 0.1.6 because there were some interface and
    configuration changes (referencing stackinfo instead of stack_info, for
    example).

--- lggr/__init__.py
+++ lggr/__init__.py
@@ -10,7 +10,7 @@ import inspect
 import traceback
 from lggr.coroutine import coroutine, coroutine_process, coroutine_thread
 
-__version__ = '0.1.5'
+__version__ = '0.2.0'
 
 DEBUG = 'DEBUG'
 INFO = 'INFO'
